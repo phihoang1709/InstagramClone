6d50115d9fc2c9236a42d2701197e458e34d28c7
denker9999 <52444982+phihoang1709@users.noreply.github.com>
2024-03-05 17:40:50 +0700

    asdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Flowbite>
+
+      <div className='w-max-full h-screen container dark:bg-slate-800'>
+      <DarkThemeToggle />
+          <p className='font-bold text-4xl dark:text-white'>Welcome to my project</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Flowbite>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import flowbitePlugin from 'flowbite/plugin';
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbitePlugin()
+  ],
+}
+
